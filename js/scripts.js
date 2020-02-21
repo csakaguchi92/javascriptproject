4d8347aae0fecd958a07e2d4928522f2c15f91a6
Claire Sakaguchi <claire.sakaguchi@gmail.com>
2020-02-21 11:11:30 -0800

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,83 @@ $(document).ready(function() {
 
     
     
-    if (skills === "info" && pet === "owl" && saturday === "library" && hallow === "wand") {
-      $('#griffindor').show();
+    if (skills === "info" && saturday === "library") {
+      $('#ravenclaw').show();
     } 
-   
-    if (skills === "friends" && pet === "owl" && saturday === "library" && hallow === "wand") {
+
+    if (skills === "info" && saturday === "forest") {
+      $('#slytherin').show();
+    } 
+
+    if (skills === "info" && saturday === "kitchen") {
+      $('#hufflepuff').show();
+    } 
+
+    if (skills === "info" && saturday === "room") {
       $('#griffindor').show();
     } 
 
-    if (skills === "secrets" && pet === "owl" && saturday === "library" && hallow === "wand") {
+  
+    if (skills === "friends" && saturday === "library") {
+      $('#hufflepuff').show();
+    } 
+
+    if (skills === "friends" && saturday === "forest") {
+      $('#slytherin').show();
+    } 
+
+    if (skills === "friends" && saturday === "kitchen") {
       $('#griffindor').show();
     } 
+
+    if (skills === "friends" && saturday === "room") {
+      $('#ravenclaw').show();
+    } 
+
     
-    if (skills === "want" && pet === "owl" && saturday === "library" && hallow === "wand") {
+
+    if (skills === "want" && saturday === "library") {
+      $('#ravenclaw').show();
+    } 
+
+    if (skills === "want" && saturday === "forest") {
       $('#slytherin').show();
     } 
-    
-    if (skills === "want" && pet === "owl" && saturday === "library" && hallow === "wand") {
+
+    if (skills === "want" && saturday === "kitchen") {
+      $('#hufflepuff').show();
+    } 
+
+    if (skills === "want" && saturday === "room") {
+      $('#griffindor').show();
+    } 
+
+
+    if (skills === "secrets" && saturday === "library") {
+      $('#griffindor').show();
+    } 
+
+    if (skills === "secrets" && saturday === "forest") {
       $('#slytherin').show();
     } 
-    
+
+    if (skills === "secrets" && saturday === "kitchen") {
+      $('#ravenclaw').show();
+    } 
+
+    if (skills === "secrets" && saturday === "room") {
+      $('#hufflepuff').show();
+    } 
+
    
+   
+
 
 
   
     
+
+
     event.preventDefault();
 
   });
